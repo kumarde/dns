6f53b0d41ad30895067466444f7114f939c3c1b6
Deepak Kumar <kumarde@umich.edu>
2018-03-07 17:11:33 -0600

    first semblance of a parsing function

--- preferred_name_syntax/src/main.rs
+++ preferred_name_syntax/src/main.rs
@@ -1,38 +1,11 @@
-extern crate rand;
-
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
+use std::env;
 
 fn main() {
-    println!("Guess the number!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    check_if_domain_is_in_preferred_name_syntax(query)
+}
 
-    loop{
-
-        let secret_number = rand::thread_rng().gen_range(1, 101);
-        println!("The secret number is: {}", secret_number);
-
-        println!("Please input your guess.");
-
-        let mut guess = String::new();
-
-        io::stdin().read_line(&mut guess)
-            .expect("Failed to read line");
-
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal => {
-                println!("You win!");
-                break;
-            }
-        }
-    }
+fn check_if_domain_is_in_preferred_name_syntax(domain: &String){
+    println!("domain is: {}", domain);
 }
