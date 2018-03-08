ded82e978ad131b1521ac7cef7bfdfa358577e17
Deepak Kumar <kumarde@umich.edu>
2018-03-07 21:15:10 -0600

    ok

--- preferred_name_syntax/target/debug/deps/preferred_name_syntax-864a37d08d6bd646.d
+++
@@ -1,5 +0,0 @@
-/Users/kumarde/Documents/research/dns/preferred_name_syntax/target/debug/deps/preferred_name_syntax-864a37d08d6bd646: main.rs
-
-/Users/kumarde/Documents/research/dns/preferred_name_syntax/target/debug/deps/preferred_name_syntax-864a37d08d6bd646.d: main.rs
-
-main.rs:
--- preferred_name_syntax/target/debug/preferred_name_syntax.d
+++
@@ -1,1 +0,0 @@
-/Users/kumarde/Documents/research/dns/preferred_name_syntax/target/debug/preferred_name_syntax: /Users/kumarde/Documents/research/dns/preferred_name_syntax/src/main.rs
